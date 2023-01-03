d47f633f18171190a8ee9a274eb8de528431fe0d
bszeidl <szeidl.balazs@hotmail.com>
2023-01-03 22:39:16 +0100

    The center and one petal of the flower is ready.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+    
+    context.beginPath();
+
+    context.arc(200,200,40,0,2.5*Math.PI);
+    context.fillStyle ='orange';
+    context.fill();
+
+    context.beginPath();
+    context.moveTo(200,160);
+    context.bezierCurveTo(150,0,250,0,200,160);
+    context.fillStyle = 'red';
+    context.fill();
+    
 
 }
 
